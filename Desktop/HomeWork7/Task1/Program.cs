3e65771d2a991c13a904eca469e9e36ad5627181
Koriakin Pavel <polo_kor@mail.ru>
2023-03-15 23:05:27 +0300

    HomeWork7

---
+++ Desktop/HomeWork7/Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+int[,] GenerateArray(int row, int col)
+{
+    int[,] array = new int[row, col]; // Создаем 2-мерный массив
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+    }
+    System.Console.WriteLine();
+}
+
+int InputUser(string msg)
+{
+    Console.Write($"{msg} -> ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int row = InputUser("Введите количество строк массива");
+int col = InputUser("Введите количество столбцов массива");
+int [,] marix = GenerateArray(row, col);
+PrintArray(marix);
